df3c0b3f61dca2e2a904d352dec5dcadda752fe4
Adel <leto.456@mail.ru>
2025-07-09 17:17:30 +0300

    Additions added ingredient model added recipe model added ingredientrecipe model

---
+++ backend/foodgram/recipes/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+from django.contrib.auth import get_user_model
+
+UserModel = get_user_model()
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=128)
+    measurement_unit = models.CharField(max_length=64)
+
+
+class Recipe(models.Model):
+    author = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+    name = models.CharField(max_length=256)
+    image = models.ImageField(upload_to='recipes')
+    text = models.TextField()
+    cooking_time = models.PositiveIntegerField()
+
+
+class IngredientRecipe(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
