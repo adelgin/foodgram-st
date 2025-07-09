c3ef8aa9284b32a71eee80b183446bbf660690cc
Adel <leto.456@mail.ru>
2025-07-09 17:46:15 +0300

    added ingredient search filter

---
+++ backend/foodgram/recipes/filters.py
@@ -0,0 +1,4 @@
+from rest_framework.filters import SearchFilter
+
+class IngredientFilter(SearchFilter):
+    search_param = 'name'
--- backend/foodgram/recipes/views.py
+++ backend/foodgram/recipes/views.py
@@ -3,10 +3,13 @@ from rest_framework import viewsets, permissions
 
 from .models import Ingredient, Recipe, IngredientRecipe
 from .serializers import IngredientSerializer
+from .filters import IngredientFilter
 
 
 class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Ingredient.objects.all()
     serializer_class = IngredientSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly, )
+    filter_backends = (IngredientFilter, )
+    search_fields = ('^name', )
     pagination_class = None
