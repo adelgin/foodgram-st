46d00514af67df1bc80ff7741f49d64b43ad6596
Adel <leto.456@mail.ru>
2025-07-10 14:46:40 +0300

    added recipe query filtering

--- backend/foodgram/recipes/filters.py
+++ backend/foodgram/recipes/filters.py
@@ -1,5 +1,6 @@
 from rest_framework.filters import SearchFilter
 from django_filters import FilterSet
+from django_filters.rest_framework import BooleanFilter
 
 from .models import Recipe
 
@@ -7,7 +8,24 @@ class IngredientFilter(SearchFilter):
     search_param = 'name'
 
 class RecipeFilter(FilterSet):
+    is_favorited = BooleanFilter(method='get_is_favorited')
+    is_in_shopping_cart = BooleanFilter(method='get_is_in_shopping_cart')
+
     class Meta:
         model = Recipe
         fields = ('author', )
+
+    def get_is_favorited(self, queryset, name, value):
+        user = self.request.user
+        if value and user.is_authenticated:
+            return queryset.filter(favorite__user=user)
+        return queryset
+    
+    def get_is_in_shopping_cart(self, queryset, name, value):
+        user = self.request.user
+        if value and user.is_authenticated:
+            return queryset.filter(shoppingcart__user=user)
+        return queryset
+
+    
 
